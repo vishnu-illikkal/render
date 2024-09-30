24d18f1cb3153aa04017f71a36bfa7d7ec9d0492
Vishnu I P <122511639+vishnu-illikkal@users.noreply.github.com>
2024-09-30 20:19:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is a demo site deployed on Render!"
+
+@app.route('/about')
+def about():
+    return "This is the about page."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
